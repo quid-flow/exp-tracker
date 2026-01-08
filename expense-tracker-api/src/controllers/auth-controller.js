dece7e04ac77b3a3451125fd09a67d051dc295be
quid-flow <quid.flow.project@gmail.com>
2026-01-08 09:42:41 +0530

    Removed Subcategory

---
+++ expense-tracker-api/src/controllers/auth-controller.js
@@ -0,0 +1,9 @@
+
+const {generateToken} = require('../utils/jwt');
+
+
+
+
+
+
+
---
+++ expense-tracker-api/src/utils/jwt.js
@@ -0,0 +1,23 @@
+
+const jwt = require('jsonwebtoken'); // Import the jsonwebtoken library
+
+
+const generateToken = (payload) => {
+  return jwt.sign(
+    payload,
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = {
+  generateToken,
+  verifyToken
+};
+
+
+
