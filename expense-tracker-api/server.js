4e643699d3d0364c2c05fbba89c0a4c49363598c
quid-flow <quid.flow.project@gmail.com>
2026-03-20 22:48:14 +0530

    .

--- expense-tracker-api/server.js
+++ expense-tracker-api/server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5174",
   "http://192.168.10.188:5174",
+  "http://172.21.17.248:5174",
   "https://exp-tracker-opal.vercel.app"
 ];
 
@@ -30,8 +31,6 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
-
 
 // routes import
 const authRoutes = require("./src/routes/auth-route");
